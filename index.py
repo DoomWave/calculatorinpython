4f4453ea24fd618b8dc9dac04e496df81f580702
DoomWave <123853739+DoomWave@users.noreply.github.com>
2024-12-09 20:21:45 -0800

    Base of the calculator started.

--- index.py
+++ index.py
@@ -6,3 +6,21 @@ class Calculator:
         
         self.root = root
         self.root .title("Calculator")
+        self.root.geometry("615x680+400+100")
+        self.root.config(bg = 'cadet blue')
+
+        self.MainFrame = Frame(self.root , bd =18, width = 600, height=670, relief=RIDGE, bg = 'powder blue')
+        self.MainFrame.grid()
+        self.WidgetFrame = Frame(self.MainFrame , bd =18, width = 590, height=660, relief=RIDGE, bg = 'cadet blue')
+        self.WidgetFrame.grid()
+        
+        
+root = Tk()
+App = Calculator(root)
+root.mainloop()
+
+
+
+
+
+
