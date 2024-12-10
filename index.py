2efcf96cc380d7527817f01636dbd1f965f704a3
DoomWave <123853739+DoomWave@users.noreply.github.com>
2024-12-09 20:02:51 -0800

    Calculator complete it.

---
+++ index.py
@@ -0,0 +1,18 @@
+operator = input("Enter Operator ( + - * /): ")
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2st number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 3))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 3))
+elif operator == "*":
+    result = num1 * num2
+    print(round(result, 3))
+elif operator == "/":
+    result = num1 * num2
+    print(round(result, 3))
+else:
+    print(f"{operator} is not a valid")
